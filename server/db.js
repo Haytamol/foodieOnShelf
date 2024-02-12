d9731ca90dd8b73e59fc562e719ee77600d453a0
Haytamol <haytammostaine2002@gmail.com>
2024-02-12 03:44:42 +0100

    changes to db.sql

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "baka",
-    host: "localhost",
-    port: 5432,
-    database: "foodieonshelf"
-})
+  connectionString: process.env.POSTGRES_URL,
+});
+
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "baka",
+//   host: "localhost",
+//   port: 5432,
+//   database: "foodieonshelf",
+// });
 
 module.exports = pool;
