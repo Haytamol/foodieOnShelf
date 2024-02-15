2d828fbb5be5c6debd2480320c716e3d8fdc9101
Haytamol <haytammostaine2002@gmail.com>
2024-02-15 20:21:35 +0100

    update db.js

--- server/db.js
+++ server/db.js
@@ -8,4 +8,18 @@ const pool = new Pool({
   database: "postgres",
 });
 
+pool.connect((err, client, release) => {
+  if (err) {
+    // Handle connection error
+    console.error("Error acquiring client", err.stack);
+  } else {
+    console.log("Connected to the database!");
+    // Do something with the client
+    // ...
+
+    // Release the client back to the pool
+    release();
+  }
+});
+
 module.exports = pool;
